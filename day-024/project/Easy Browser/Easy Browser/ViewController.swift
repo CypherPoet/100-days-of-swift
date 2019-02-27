86cd0fd90b6c55c8a21a9637b92568c011044476
CypherPoet <CypherPoet@gmail.com>
2019-02-27 17:56:06 -0500

    copy over original project 4

---
+++ day-024/project/Easy Browser/Easy Browser/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Brian Sipple on 1/16/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var urlsToChoose = [URL]()
+    
+    let siteNames = [
+        "hackingwithswift.com",
+        "google.com",
+        "yalls.org",
+        "developer.apple.com/develop"
+    ]
+    
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        view = webView
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        webView.allowsBackForwardNavigationGestures = true
+        
+        setupNavigationBar()
+        setupToolbar()
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for siteName in siteNames {
+                if host.contains(siteName) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
+    
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    
+    func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "🌐",
+            style: .plain,
+            target: self,
+            action: #selector(openSitePicker)
+        )
+    }
+    
+    
+    func setupToolbar() {
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        
+        let progressButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    
+    @objc func openSitePicker() {
+        let alertController = UIAlertController(
+            title: "Surf the Web!",
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        
+        for siteName in siteNames {
+            alertController.addAction(UIAlertAction(title: siteName, style: .default, handler: openPage))
+        }
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        alertController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        
+        present(alertController, animated: true)
+    }
+    
+    
+    func openPage(action: UIAlertAction) {
+        let pageURL = URL(string: "https://\(action.title!)")!
+        
+        webView.load(URLRequest(url: pageURL))
+    }
+}
+
