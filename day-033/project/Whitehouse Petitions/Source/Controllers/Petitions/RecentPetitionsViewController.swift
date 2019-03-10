5de07346ba996494cd4c4e6b0408ee1487b30bf4
CypherPoet <CypherPoet@gmail.com>
2019-03-10 01:09:01 -0500

    initialize project

---
+++ day-033/project/Whitehouse Petitions/Source/Controllers/Petitions/RecentPetitionsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Brian Sipple on 3/10/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class RecentPetitionsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+}
+
